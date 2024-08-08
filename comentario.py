a1b90e240f4e4c223cf46eabfe16f19c4ba73566
Izzat <jhonatan.morais2001@gmail.com>
2024-08-08 15:24:45 -0300

    primeiro commit

---
+++ comentario.py
@@ -0,0 +1,80 @@
+import selenium
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
+import datetime
+
+def enviar_mensagem(driver, mensagem):
+    try:
+        # Localiza o campo de comentario pelo nome da Classe 
+        comentario = driver.find_element(By.CLASS_NAME, "xaqnwrm")
+        comentario.click()
+        sleep(5)
+        comentario = driver.find_element(By.CLASS_NAME, "xaqnwrm")
+        comentario.send_keys(mensagem)
+        sleep(10)
+        # Clicando em enviar
+        enviar = driver.find_element(By.XPATH, '//div[@class="x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x1lku1pv x1a2a7pz x6s0dn4 xjyslct x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 x1ypdohk x1f6kntn xwhw2v2 xl56j7k x17ydfre x2b8uid xlyipyv x87ps6o x14atkfc xcdnw81 x1i0vuye xjbqb8w xm3z3ea x1x8b98j x131883w x16mih1h x972fbf xcfux6l x1qhh985 xm0m39n xt0psk2 xt7dq6l xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x1n5bzlp x173jzuc x1yc6y37"]')
+        enviar.click()
+        hora = datetime.datetime.now().strftime("%H:%M:%S")
+        print(f"Mensagem enviada {hora}")
+        sleep(8)
+        driver.refresh()
+    except NoSuchElementException as e:
+        print(f"não encontrei o campo: {e}")
+    except Exception as e:
+        print(f"Erro ao enviar a mensagem: {e}")
+
+def main():
+    option = Options()
+    option.add_argument('--start-maximized')
+    driver = webdriver.Chrome(options=option)
+    driver.get('https://instagram.com')
+    sleep(15)
+
+    # Login do Instagram
+    while True:
+        try:
+            #inserindo login
+            login = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+            login.send_keys("seu login")
+            #inserindo senha
+            senha = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
+            senha.send_keys("sua senha")
+            # localizando o botão entrar e clicando em entrar
+            entrar = driver.find_element(By.XPATH, '//div[@class="x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1xmf6yo x1e56ztr x540dpk x1m39q7l x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1"]')
+            entrar.click()
+            print("login efetuado")
+            break
+            sleep(8)
+        except NoSuchElementException as e:
+            print(f"não foi possivel logar: {e}")
+
+    # Aguardar a pagina carregar
+    sleep(10)
+
+    # acessando a postagem
+    while True:
+        try:
+            postagem = driver.get('https://www.instagram.com/p/C95sbWrpG7E/')
+            print("encontrei a postagem!")
+            break
+        except:
+            print("Não encontrei a postagem")
+
+    # Esperando a pagina carregar
+    sleep(10)
+
+    # Definindo o tempo e a mensagem
+    tempo_intervalo = 320  # Intervalo de tempo em segundos
+    mensagem = "Eu posso eu consigo"
+
+    # Loop de envio de mensagens
+    while True:
+        enviar_mensagem(driver, mensagem)
+        sleep(tempo_intervalo)
+
+if __name__ == "__main__":
+    main()
---
+++ raspagem.py
@@ -0,0 +1,54 @@
+import selenium
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+import requests
+from bs4 import BeautifulSoup
+
+option = Options()
+option.add_argument('--start-maximized')
+driver = webdriver.Chrome(options=option)
+driver.get('https://instagram.com')
+
+while True:
+    try:
+        login = driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[1]/div/label/input')
+        login.send_keys("seu login") #login
+        senha = driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[2]/div/label/input')
+        senha.send_keys("sua senha") #senha
+        entrar = driver.find_element(By.XPATH,'//div[@class="x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1xmf6yo x1e56ztr x540dpk x1m39q7l x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1"]')
+        entrar.click()
+        print("login efetuado")
+        break
+        
+    except:
+        print("erro")
+        sleep(1)
+
+# Aguardar a pagina carregar
+sleep(8)       
+
+# Acessar a página do perfil
+driver.get('https://www.instagram.com/johnizzat_/')
+sleep(6)
+
+# Clicar em seguidores
+while True:
+    try:
+        seguidores = driver.find_element(By.XPATH,"//a[@class='x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _alvs _a6hd']")
+        seguidores.click()
+        print("Consegui Clicar!")
+        break
+    except:
+        print('elemento encontrado!!')
+        sleep(6)
+
+# Raspagem de dados
+segudiores = driver.find_elements(class_="x1dm5mii x16mil14 xiojian x1yutycm x1lliihq x193iq5w xh8yej3")
+print(seguidores)
+
+
+
+
+input('Z')
