919e0dc93eef49d463a895d78271fce492b119ba
Izzat <jhonatan.morais2001@gmail.com>
2024-08-20 11:25:33 -0300

    Atulização 20/08/2024 veja no Release

--- comentario.py
+++ comentario.py
@@ -41,7 +41,7 @@ def main():
             login.send_keys("Seu login")
             #inserindo senha
             senha = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
-            senha.send_keys("Sua senha")
+            senha.send_keys("Sua senhaa")
             # localizando o botão entrar e clicando em entrar
             entrar = driver.find_element(By.XPATH, '//div[@class="x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1xmf6yo x1e56ztr x540dpk x1m39q7l x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1"]')
             entrar.click()
